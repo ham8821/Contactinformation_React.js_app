274a01b7f0c5400c26c692e5cd821d74e2c54cb1
ham8821 <ham38538821@gmail.com>
2019-04-18 18:46:59 +1200

    searching function_ added

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contact from './Contact';
+import './../App.css';
+class App extends React.Component {
+    render(){
+        return (
+                <Contact/>
+        );
+    }
+}
+
+export default App;
